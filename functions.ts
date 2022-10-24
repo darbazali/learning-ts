61ba2ca2d10c7c1bf82a77d0ce902bc492c4ce8d
Darbaz Ali <darbaz.me@gmail.com>
2022-10-24 16:51:32 +0300

    function return type

---
+++ functions.ts
@@ -0,0 +1,10 @@
+function addNumbers(a: number, b: number): number {
+    return a + b
+}
+
+console.log(addNumbers(1, 2)) // returns 3
+
+const addStrings = (str1: string, str2: string = ""): string =>
+    `${str1} ${str2}`
+
+console.log(addStrings("a", "b")) // returns a b
