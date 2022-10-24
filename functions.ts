377b3aae3cd87ddb66a182da25a1b9abb6bcf42a
Darbaz Ali <darbaz.me@gmail.com>
2022-10-24 16:53:16 +0300

    Union type

--- functions.ts
+++ functions.ts
@@ -8,3 +8,8 @@ const addStrings = (str1: string, str2: string = ""): string =>
     `${str1} ${str2}`
 
 console.log(addStrings("a", "b")) // returns a b
+
+const format = (title: string, param: string | number): string =>
+    `${title} ${param}`
+
+console.log(format("Hunter", 2))
