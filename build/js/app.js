c1a271d7555460e8fdb0b5a8eb5ecdb27634bf57
Interceptor <darbaz.me@gmail.com>
2023-09-11 21:25:00 +0300

    project setup

---
+++ build/js/app.js
@@ -0,0 +1,6 @@
+"use strict";
+const { log, clear } = console;
+clear();
+let a = 12;
+let b = 6;
+log(a / b);
---
+++ src/app.ts
@@ -0,0 +1,9 @@
+const { log, clear } = console
+
+clear()
+
+let a: number = 12
+let b: number = 6
+
+
+log(a / b) 
